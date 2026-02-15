5f57407b0eb3c7cc676c06f9e361dcbf833c1540
MSufyan62 <83629255+MSufyan62@users.noreply.github.com>
2026-02-15 11:43:54 +0000

    Added PaniPuri

--- FirstSampleFolder/Biryani.cpp
+++ FirstSampleFolder/Biryani.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "I Like Biryani!" << endl<<endl;
+    cout << "I Likea Biryani!" << endl<<endl;
     return 0;
 }
---
+++ FirstSampleFolder/PaniPuri.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    std::cout<<"I like Pani Puri alot" <<std::endl;
+    return 0;
+}
